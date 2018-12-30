722901c21c205f016bee0c7e8cb523690075e7d0
Alex Gabites <alex@netspeed.net.nz>
2018-12-31 12:58:49 +1300

    Changes to allow converter to be used outside of package

--- maps/run.js
+++ maps/run.js
@@ -1,4 +1,12 @@
 const { ConvertSVGs } = require('./convert-svgs');
 
-const convertSVGs = new ConvertSVGs();
+const SVG_MAP_FILES = `${__dirname}\\svg`;
+const JSON_MAP_FILES = `${__dirname}\\json`;
+const EXPORTS_FILE = `${__dirname}\\..\\src\\maps.js`;
+
+const convertSVGs = new ConvertSVGs({
+  inputSvgsPath: SVG_MAP_FILES,
+  outputJsonPath: JSON_MAP_FILES,
+  generatedExportsFilePath: EXPORTS_FILE,
+});
 convertSVGs.run();
