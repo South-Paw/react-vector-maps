2d10456e5c12a2a4a19b7c96657728cb4a5f6932
Alex Gabites <alex@netspeed.net.nz>
2022-09-29 17:46:52 +1300

    Update project config and dependencies
    - Changed to use tsup for builds (replaces tsdx)
    - Removed unused storybook commands from scripts
    - Updated eslint config
    - Updated jest config
    - Updated ts config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/test/setupJest.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/docs/'],
 };
